429fb565dd727e96d3e02ae54e013cf287798f1b
Đinh Khắc Nhật Trường <truong15022006@gmail.com>
2026-06-17 02:41:24 +0700

    feat add MambaVisionUNet to the module exports

--- net/__init__.py
+++ net/__init__.py
@@ -1,13 +1,14 @@
 from .unet import UNet5ch
 from .resnet_unet import ResNetUNet
 from .mobilenet_unet import MobileNetUNet
+from .mambavision_unet import MambaVisionUNet
 from .physics import compute_physics_maps, estimate_background_light, estimate_transmission_udcp
 from .registry import build_model, parse_model_variant, ALL_MODEL_NAMES
-
 __all__ = [
     "UNet5ch",
     "ResNetUNet",
     "MobileNetUNet",
+    "MambaVisionUNet",
     "compute_physics_maps",
     "estimate_background_light",
     "estimate_transmission_udcp",
